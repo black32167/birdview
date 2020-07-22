fd723703a7f8130358ef66e21b72a12165231681
black <psgrepjava@gmail.com>
2020-07-22 22:44:15 +1000

    using ItemsIterable in GithubClient

--- birdview/src/main/kotlin/org/birdview/source/github/GithubClient.kt
+++ birdview/src/main/kotlin/org/birdview/source/github/GithubClient.kt
@@ -1,48 +1,20 @@
 package org.birdview.source.github
 
 import org.birdview.config.BVGithubConfig
-import org.birdview.source.BVTaskListsDefaults
+import org.birdview.source.ItemsIterable
+import org.birdview.source.ItemsPage
 import org.birdview.source.github.model.*
-import org.birdview.utils.BVConcurrentUtils
 import org.birdview.utils.remote.BasicAuth
+import org.birdview.utils.remote.ResponseValidationUtils
 import org.birdview.utils.remote.WebTargetFactory
 import org.slf4j.LoggerFactory
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
-import java.util.concurrent.Callable
-import java.util.concurrent.Executors
+import javax.ws.rs.core.Response
 
-//class GithubIssuesFilter(
-//        val issueState: String?,
-//        val since: ZonedDateTime?,
-//        val user:String?
-//)
 class GithubClient(
-        private val githubConfig: BVGithubConfig,
-        private val sourceConfig: BVTaskListsDefaults
+        private val githubConfig: BVGithubConfig
 ) {
     private val log = LoggerFactory.getLogger(GithubClient::class.java)
-    private val executor = Executors.newCachedThreadPool(BVConcurrentUtils.getDaemonThreadFactory())
-
-    private fun getCurrentUserIssues(issueState: String, since:ZonedDateTime):List<GithubIssue> =
-        getTarget()
-            ?.let { githubRestTarget->
-                val githubIssuesResponse = githubRestTarget.path("issues")
-                        .queryParam("filter", "created")
-                        .queryParam("state", issueState)
-                        .queryParam("per_page", sourceConfig.getMaxResult())
-                        .queryParam("since", since.format(DateTimeFormatter.ISO_DATE_TIME))
-                        .request()
-                        .get()
-
-                if(githubIssuesResponse.status != 200) {
-                    throw RuntimeException("Error reading Github issues: ${githubIssuesResponse.readEntity(String::class.java)}")
-                }
-
-                return githubIssuesResponse.readEntity(Array<GithubIssue>::class.java).toList()
-            }
-            ?: listOf<GithubIssue>()
-
+    private val issuesPerPage = 50
 
     fun getIssueComments(pullRequest: GithubPullRequest) =
         getTarget(pullRequest.comments_url)
@@ -84,51 +56,32 @@ class GithubClient(
                     ?.asList()
                     ?: emptyList()
 
-    fun findIssues(query:String):List<GithubIssue> {
+    fun findIssues(query:String): Iterable<GithubIssue> {
         log.info("Running github query '{}'", query)
         return getTarget()
                 ?.path("search")
                 ?.path("issues")
                 ?.queryParam("q", query)
-//                ?.also {
-//                    println("Url:${it.uri}")
-//                }
+                ?.queryParam("per_page", issuesPerPage)
                 ?.request()
                 ?.get()
-                ?.also {
-                    if (it.status != 200) {
-                        throw java.lang.RuntimeException("Status:${it.status}, message=${it.readEntity(String::class.java)}")
+                ?.let { resp ->
+                    ItemsIterable(mapIssuesPage (resp)) { url ->
+                        log.info("Loading github issues next page: {}", url)
+                        getTarget(url)?.request()?.get()?.let (this::mapIssuesPage)
                     }
-//                    println(it.readEntity(String::class.java))
-//                    println()
                 }
-                ?.readEntity(GithubSearchIssuesResponse::class.java)?.items
-                ?.asList()
-                ?: listOf()
+                ?: ItemsIterable<GithubIssue, String>()
     }
 
-    private fun getUserIdByEMail(email: String): String =
-            getTarget()
-                    ?.path("search")
-                    ?.path("users")
-                    ?.queryParam("q", "${email} in:email")
-                    ?.request()
-                    ?.get()
-                    ?.also {
-                        println("Status:${it.status}")
-                        println(it.readEntity(String::class.java))
-                        println("")
+    private fun mapIssuesPage(response: Response): ItemsPage<GithubIssue, String> =
+            response
+                    .also (ResponseValidationUtils::validate)
+                    .let { resp ->
+                        ItemsPage (
+                                resp.readEntity(GithubSearchIssuesResponse::class.java).items.asList(),
+                                resp.links.firstOrNull { "next" == it.rel } ?.uri ?.toString())
                     }
-                    ?.readEntity(String::class.java)
-                    ?:""
-
-    fun getCurrentUserPullRequests(issueState: String, since: ZonedDateTime):List<GithubPullRequest> =
-            getCurrentUserIssues(issueState, since)
-                    .mapNotNull { issue -> // Get Pull Requests
-                        issue.pull_request?.url
-                                ?.let { pr_url -> executor.submit(Callable<GithubPullRequest> { getPullRequest(pr_url) }) }
-                    }
-                    .map { it.get() }
 
     private fun getConfig(): BVGithubConfig?  = githubConfig
 
--- birdview/src/main/kotlin/org/birdview/source/github/GithubClientProvider.kt
+++ birdview/src/main/kotlin/org/birdview/source/github/GithubClientProvider.kt
@@ -1,15 +1,10 @@
 package org.birdview.source.github
 
 import org.birdview.config.BVGithubConfig
-import org.birdview.config.BVUsersConfigProvider
-import org.birdview.source.BVTaskListsDefaults
 import javax.inject.Named
 
 @Named
-class GithubClientProvider(
-        private val taskListDefaults: BVTaskListsDefaults,
-        private val usersConfigProvider: BVUsersConfigProvider
-) {
+class GithubClientProvider {
     fun getGithubClient(githubConfig: BVGithubConfig) =
-            GithubClient(githubConfig, taskListDefaults)
+            GithubClient(githubConfig)
 }
