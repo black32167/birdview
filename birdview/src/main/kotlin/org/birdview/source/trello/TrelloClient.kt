236b6a41fcae993972ee351e0ea57ba1b190c79c
black <psgrepjava@gmail.com>
2020-07-23 23:04:40 +1000

    using ItemsIterable in TrelloClient

--- birdview/src/main/kotlin/org/birdview/source/trello/TrelloClient.kt
+++ birdview/src/main/kotlin/org/birdview/source/trello/TrelloClient.kt
@@ -2,29 +2,32 @@ package org.birdview.source.trello
 
 import org.birdview.config.BVTrelloConfig
 import org.birdview.source.BVTaskListsDefaults
+import org.birdview.source.ItemsIterable
+import org.birdview.source.ItemsPage
 import org.birdview.source.trello.model.TrelloBoard
 import org.birdview.source.trello.model.TrelloCard
 import org.birdview.source.trello.model.TrelloCardsSearchResponse
 import org.birdview.source.trello.model.TrelloList
+import org.birdview.utils.remote.ResponseValidationUtils
 import org.birdview.utils.remote.WebTargetFactory
 import org.slf4j.LoggerFactory
+import javax.ws.rs.core.Response
 
 class TrelloClient(private val trelloConfig: BVTrelloConfig,
                    private val sourceConfig: BVTaskListsDefaults) {
     private val log = LoggerFactory.getLogger(TrelloClient::class.java)
-    fun getCards(query:String): List<TrelloCard> {
-        log.info("Running Trello query '{}'", query)
-        val trelloCardsResponse = getTarget().path("search")
-                .queryParam("query", query)
-                .queryParam("partial", true)
-                .queryParam("cards_limit", sourceConfig.getMaxResult())
-                .request()
-                .get()
+    private val cardsPerPage = 50
 
-        if(trelloCardsResponse.status != 200) {
-            throw RuntimeException("Error reading Trello cards: ${trelloCardsResponse.readEntity(String::class.java)}")
-        }
-        return trelloCardsResponse.readEntity(TrelloCardsSearchResponse::class.java).cards.toList()
+    fun getCards(query:String): Iterable<TrelloCard> {
+        log.info("Running Trello query '{}'", query)
+        return searchTrelloCards(query, 0)
+                .let { resp ->
+                    ItemsIterable(mapCardsPage(resp, 0)) { page ->
+                        log.info("Loading trello issues next page: {}", page)
+                        searchTrelloCards(query, page)
+                                ?.let { mapCardsPage(it, page) }
+                    }
+                }
     }
 
     fun getBoards(boardIds: List<String>): List<TrelloBoard> =
@@ -56,6 +59,24 @@ class TrelloClient(private val trelloConfig: BVTrelloConfig,
             }
             response.readEntity(TrelloList::class.java)
         }
+
+    private fun searchTrelloCards(query: String, cardsPage: Int) = getTarget().path("search")
+            .queryParam("query", query)
+            .queryParam("partial", true)
+            .queryParam("cards_limit", cardsPerPage)
+            .queryParam("cards_page", cardsPage)
+            .request()
+            .get()
+
+    private fun mapCardsPage(response: Response, page:Int): ItemsPage<TrelloCard, Int> =
+            response
+                    .also (ResponseValidationUtils::validate)
+                    .let { resp ->
+                        val cardsResponse = resp.readEntity(TrelloCardsSearchResponse::class.java)
+                        ItemsPage (
+                                cardsResponse.cards.toList(),
+                                page + 1)
+                    }
 
     private fun getTarget() = WebTargetFactory(trelloConfig.baseUrl)
             .getTarget("/1")
