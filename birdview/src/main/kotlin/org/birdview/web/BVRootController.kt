0eb63649980fccf9234f76c2638d196e511e73a8
black <psgrepjava@gmail.com>
2020-10-12 23:05:58 +1100

    BVAdminController -> BVRootController

---
+++ birdview/src/main/kotlin/org/birdview/web/BVRootController.kt
@@ -0,0 +1,19 @@
+package org.birdview.web
+
+import org.birdview.security.UserContext
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.ModelAndView
+
+@Controller
+@RequestMapping("/")
+class BVRootController {
+    @GetMapping
+    fun navigateHome() =
+            if (UserContext.isAdmin()) {
+                ModelAndView("redirect:${BVWebPaths.SECRETS}")
+            } else {
+                ModelAndView("redirect:${BVWebPaths.EXPLORE}")
+            }
+}
