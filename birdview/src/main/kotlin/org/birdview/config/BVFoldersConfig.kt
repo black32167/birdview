ddea85399fa7f34ba1776b3e14b4711a924f3116
black <psgrepjava@gmail.com>
2020-10-14 22:37:17 +1100

    BVRuntimeConfig.kt -> BVFoldersConfig.kt

---
+++ birdview/src/main/kotlin/org/birdview/config/BVFoldersConfig.kt
@@ -0,0 +1,32 @@
+package org.birdview.config
+
+import org.springframework.beans.factory.annotation.Value
+import java.nio.file.Files
+import java.nio.file.Path
+import javax.inject.Named
+
+@Named
+class BVFoldersConfig (
+        @Value("\${config.location}") private val configFolder: Path) {
+    val sourcesSharedSecretsConfigsFolder = Files.createDirectories(configFolder.resolve("sources"))
+    val usersConfigFolder = Files.createDirectories(configFolder.resolve("users"))
+    val oauthTokenDir = Files.createDirectories(configFolder.resolve("tokens"))
+
+    fun getUserConfigFolder(userName: String): Path =
+            usersConfigFolder.resolve(userName)
+
+    fun getUserSourceConfigsFolder(userName: String): Path =
+            getUserConfigFolder(userName).resolve("sources")
+                    .also (this::createSingleDirectoryIfNotExists)
+
+    fun getUserSourceTokensFolder(userName: String): Path =
+            Files.createDirectories(getUserConfigFolder(userName).resolve("tokens"))
+                    .also (this::createSingleDirectoryIfNotExists)
+
+    private fun createSingleDirectoryIfNotExists(path:Path) {
+        if(!Files.exists(path)) {
+            Files.createDirectory(path)
+        }
+    }
+
+}
