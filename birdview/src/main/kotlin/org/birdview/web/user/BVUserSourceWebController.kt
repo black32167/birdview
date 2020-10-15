b4fdf7763811cd1a9ba70577eba046c7081a5620
black <psgrepjava@gmail.com>
2020-10-15 22:04:17 +1100

    redundant class removak

--- birdview/src/main/kotlin/org/birdview/web/user/BVUserSourceWebController.kt
+++
@@ -1,8 +0,0 @@
-package org.birdview.web.user
-
-class BVUserSourceWebController {
-    class UserSourceSettingView (
-            val sourceUserId: String,
-            val sourceUserName: String,
-            val enabled: Boolean)
-}
