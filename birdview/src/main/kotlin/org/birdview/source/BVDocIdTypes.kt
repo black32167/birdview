064f04f8b9308e54e531eb6f30fde4ba2c1423cd
black <psgrepjava@gmail.com>
2020-11-01 21:57:15 +1100

    confluence model extended

--- birdview/src/main/kotlin/org/birdview/source/BVDocIdTypes.kt
+++ birdview/src/main/kotlin/org/birdview/source/BVDocIdTypes.kt
@@ -1,6 +1,7 @@
 package org.birdview.source
 
 object BVDocIdTypes {
+    const val CONFLUENCE_PAGE_URL_TYPE = "confluencePageUrl"
     const val GDRIVE_FILE_TYPE = "gDriveFile"
     const val GITHUB_ID = "githubId"
     const val JIRA_KEY_TYPE = "jiraKey"
--- birdview/src/main/kotlin/org/birdview/source/confluence/model/ConfluenceSearchItem.kt
+++ birdview/src/main/kotlin/org/birdview/source/confluence/model/ConfluenceSearchItem.kt
@@ -1,5 +1,8 @@
 package org.birdview.source.confluence.model
 
 class ConfluenceSearchItem (
-        val title: String
+        val title: String,
+        val url: String,
+        val lastModified: String,
+        val excerpt: String?
 )
