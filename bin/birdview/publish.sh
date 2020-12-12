9e5377b3dd551b1f26db95a97a6206d593728f72
black <psgrepjava@gmail.com>
2020-12-12 21:41:53 +1100

    scripts updated

--- bin/birdview/publish.sh
+++ bin/birdview/publish.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
+set -euo pipefail
+
 SCRIPT_DIR="${BASH_SOURCE%/*}"
 . "${SCRIPT_DIR}/include.sh"
+
+dbuild() {
+	${SCRIPT_DIR}/build.sh docker
+}
 
 dpublish() {
 	docker push "${IMAGE_NAME}"
 }
 
+gpush() {
+  git push
+}
+
 case "${1}" in
 docker)
-  dpublish
+  dbuild
+  gpush
+  #dpublish
   ;;
 *)
   echo "Usage:"
