5f2fbe9aabdb8b48565370e692950bde2e8092e8
black <psgrepjava@gmail.com>
2021-01-30 00:35:06 +1100

    docker-compose

--- bin/birdview/run.sh
+++ bin/birdview/run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 SCRIPT_DIR="${BASH_SOURCE%/*}"
-. "${SCRIPT_DIR}/include.sh"
 
-: "${HOST_CONFIG_FOLDER:=${HOME}/codes}"
-
-docker run --rm -v "${HOST_CONFIG_FOLDER}":/config "${IMAGE_NAME}" "${@}"
+docker-compose -f "${SCRIPT_DIR}/docker-compose.yml" up
