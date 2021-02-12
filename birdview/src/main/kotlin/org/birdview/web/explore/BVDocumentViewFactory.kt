f19bc15b33ef8f1a38c9c1c6b043dc288a58f682
black <psgrepjava@gmail.com>
2021-02-12 23:56:27 +1100

    getting timezone from user settings

--- birdview/src/main/kotlin/org/birdview/web/explore/BVDocumentViewFactory.kt
+++ birdview/src/main/kotlin/org/birdview/web/explore/BVDocumentViewFactory.kt
@@ -1,14 +1,14 @@
 package org.birdview.web.explore
 
 import org.birdview.analysis.BVDocument
+import org.birdview.user.BVLoggedUserSettingsProvider
 import org.birdview.utils.BVDateTimeUtils
 import org.birdview.web.explore.model.BVDocumentView
 import java.time.ZoneId
 import javax.inject.Named
 
 @Named
-class BVDocumentViewFactory {
-    val displayTimeZone = ZoneId.of("Australia/NSW") // TODO
+class BVDocumentViewFactory(private val userSettings: BVLoggedUserSettingsProvider) {
     fun create(doc: BVDocument) : BVDocumentView =
             BVDocumentView(
                     internalId = doc.internalId,
@@ -18,10 +18,13 @@ class BVDocumentViewFactory {
                     status = doc.status?.name ?: "???",
                     title = doc.title,
                     updated = BVDateTimeUtils.format(
-                            doc.updated?.atZoneSameInstant(displayTimeZone), "dd-MM-yyyy"),
+                            doc.updated?.atZoneSameInstant(getDisplayTimezoneId()), "dd-MM-yyyy"),
                     key = doc.key,
                     lastUpdater = doc.operations.firstOrNull()
                             ?.run { authorDisplayName?:author },
                     priority = doc.priority
             )
+
+        private fun getDisplayTimezoneId() =
+                ZoneId.of(userSettings.getTimezoneId())
 }
