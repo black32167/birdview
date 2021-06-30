9e872a04c2a2c9e645577f5781d9165b3b860975
black <psgrepjava@gmail.com>
2021-06-30 23:22:16 +1000

    BVFireUserStorage and related

---
+++ birdview/src/main/kotlin/org/birdview/storage/firebase/BVFireUserStorage.kt
@@ -0,0 +1,43 @@
+package org.birdview.storage.firebase
+
+import org.birdview.BVProfiles
+import org.birdview.storage.BVUserStorage
+import org.birdview.storage.model.BVUserSettings
+import org.springframework.context.annotation.Profile
+import javax.inject.Named
+
+@Profile(BVProfiles.FIRESTORE)
+@Named
+class BVFireUserStorage(clienProvider: BVFirebaseClientProvider): BVUserStorage {
+    private val userCollectionRef = clienProvider.getClientForCollection("users")
+
+    override fun listUserNames(): List<String> {
+        return userCollectionRef.listDocuments()
+            .map { doc -> doc.id }
+    }
+
+    override fun create(userName: String, userSettings: BVUserSettings) {
+        userCollectionRef.document(userName).set(userSettings)
+    }
+
+    override fun update(userName: String, userSettings: BVUserSettings) {
+        userCollectionRef.document(userName).set(userSettings)
+    }
+
+    override fun getUserSettings(userName: String): BVUserSettings =
+        userCollectionRef
+            .document(userName).get().get()
+            .toObject(BVUserSettings::class.java)!!
+
+    override fun updateUserStatus(userName: String, enabled: Boolean) {
+        userCollectionRef
+            .document(userName)
+            .update(BVUserSettings::enabled.name, true)
+    }
+
+    override fun delete(userName: String) {
+        userCollectionRef
+            .document(userName)
+            .delete()
+    }
+}
