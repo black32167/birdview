effae220bbbaee24be59d3404ac390eafb72b6e3
black <psgrepjava@gmail.com>
2021-08-14 15:46:53 +1000

    +ParameterAuth

--- birdview/src/main/kotlin/org/birdview/utils/remote/ApiAuth.kt
+++ birdview/src/main/kotlin/org/birdview/utils/remote/ApiAuth.kt
@@ -3,3 +3,4 @@ package org.birdview.utils.remote
 sealed class ApiAuth
 class BearerAuth(val bearerToken: String): ApiAuth()
 class BasicAuth(val user: String, val password: String): ApiAuth()
+class ParameterAuth(val key: String, val token: String): ApiAuth()
